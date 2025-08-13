99e17bc8ecc7c93e1e5da37583661466feea3df0
snouz <snooz-x@hotmail.com>
2025-08-13 16:15:19 +0200

    simulation chamber improvements

--- prototypes/simulation_chamber.lua
+++ prototypes/simulation_chamber.lua
@@ -46,247 +46,191 @@ data:extend(
 
 
 
-
-{
-  type = "furnace",
-  name = "simulation_chamber",
-  cant_insert_at_source_message_key = "inventory-restriction.cant-be-simulated",
-  icon = icons .. "simulation_chamber.png",
-  flags = {"placeable-neutral","placeable-player", "player-creation"},
-  minable = {mining_time = 0.2, result = "simulation_chamber"},
-  fast_replaceable_group = "simulation_chamber",
-  max_health = 300,
-  corpse = "chemical-plant-remnants",
-  dying_explosion = "chemical-plant-explosion",
-  icon_draw_specification = {shift = {0, -0.3}},
-  circuit_wire_max_distance = assembling_machine_circuit_wire_max_distance,
-  circuit_connector = circuit_connector_definitions["chemical-plant"],
-  collision_box = {{-(ent_size/2 -0.3), -(ent_size/2 -0.3)}, {(ent_size/2 -0.3), (ent_size/2 -0.3)}},
-  selection_box = {{-ent_size/2, -ent_size/2}, {ent_size/2, ent_size/2}},
-  damaged_trigger_effect = hit_effects.entity(),
-  drawing_box_vertical_extension = 0.4,
-  --module_slots = 3,
-  allowed_effects = {"speed", "pollution", "quality"},
-  source_inventory_size = 1,
-  result_inventory_size = 6,
-  crafting_speed = 0.1,
-
-  graphics_set =
   {
+    type = "corpse",
+    name = "simulation_chamber-remnants",
+    icon = icons .. "simulation_chamber-remnants.png",
+    hidden_in_factoriopedia = true,
+    flags = {"placeable-neutral", "not-on-map"},
+    subgroup = "production-machine-remnants",
+    order = "a-a-a",
+    selection_box = {{-ent_size/2, -ent_size/2}, {ent_size/2, ent_size/2}},
+    tile_width = 3,
+    tile_height = 3,
+    selectable_in_game = false,
+    time_before_removed = 60 * 60 * 15, -- 15 minutes
+    expires = false,
+    final_render_layer = "remnants",
     animation = 
     {
-      layers =
-      {
-        {
-          filename = entity .. "simulation_chamber/simulation_chamber_base.png",
-          width = 512,
-          height = 512,
-          --frame_count = 1,
-          --line_length = 1,
-          --shift = util.by_pixel(0.5, -9),
-          scale = 0.5,
-        },
-        {
-          filename = entity .. "simulation_chamber/simulation_chamber_shadow.png",
-          width = 512,
-          height = 512,
-          --frame_count = 1,
-          --line_length = 1,
-          --shift = util.by_pixel(0.5, -9),
-          scale = 0.5,
-          draw_as_shadow = true,
-        },
-        --[[
-        {
-          filename = "__base__/graphics/entity/chemical-plant/chemical-plant.png",
-          width = 220,
-          height = 292,
-          frame_count = 24,
-          line_length = 12,
-          shift = util.by_pixel(0.5, -9),
-          scale = 0.5
-        },
-        {
-          filename = "__base__/graphics/entity/chemical-plant/chemical-plant-shadow.png",
-          width = 312,
-          height = 222,
-          repeat_count = 24,
-          shift = util.by_pixel(27, 6),
-          draw_as_shadow = true,
-          scale = 0.5
-        }
-        ]]
-      }
-    },
-    working_visualisations =
-    {
-      {
-        animation =
-        {
-          filename = entity .. "simulation_chamber/simulation_chamber_screen_anim.png",
-          width = 64,
-          height = 64,
-          frame_count = 255, -- max is 255
-          line_length = 32,
-          animation_speed = 1,
-          shift = util.by_pixel_hr(-128, -112),
-          scale = 0.5,
-          draw_as_glow = true,
-        },
-      },
-      {
-        animation =
-        {
-          filename = entity .. "simulation_chamber/simulation_chamber_glow.png",
-          width = 512,
-          height = 512,
-          --frame_count = 255, -- max is 255
-          --line_length = 32,
-          --animation_speed = 0.04,
-          --shift = util.by_pixel_hr(-128, -112),
-          scale = 0.5,
-          draw_as_glow = true,
-          blend_mode = "additive",
-        },
-      },
-      {
-        apply_recipe_tint = "primary",
-        animation =
-        {
-          filename = entity .. "simulation_chamber/layer1.png",
-          width = 512,
-          height = 512,
-          scale = 0.5,
-        },
-      },
-      {
-        apply_recipe_tint = "secondary",
-        animation =
-        {
-          filename = entity .. "simulation_chamber/layer2.png",
-          width = 512,
-          height = 512,
-          scale = 0.5,
-        },
-      },
-      {
-        apply_recipe_tint = "tertiary",
-        animation =
-        {
-          filename = entity .. "simulation_chamber/layer3.png",
-          width = 512,
-          height = 512,
-          scale = 0.5,
-        },
-      },
-      {
-        apply_recipe_tint = "quaternary",
-        animation =
-        {
-          filename = entity .. "simulation_chamber/layer4.png",
-          width = 512,
-          height = 512,
-          scale = 0.5,
-        },
-      },
-    }
-  },
-  impact_category = "metal-large",
-  open_sound = {filename = "__base__/sound/open-close/fluid-open.ogg", volume = 0.55},
-  close_sound = {filename = "__base__/sound/open-close/fluid-close.ogg", volume = 0.54},
-  working_sound =
-  {
-    sound = {
-      filename = tssounds .. "simulation_chamber_working_sound.ogg",
-      volume = 0.7,
-    },
-    fade_in_ticks = 10,
-    fade_out_ticks = 40,
-    max_sounds_per_prototype = 4,
-    idle_sound = {
-      filename = tssounds .. "simulation_chamber_working_sound_idle.ogg",
-      volume = 0.7,
+      filename = entity .. "simulation_chamber/simulation_chamber_remnants.png",
+      width = 512,
+      height = 512,
+      --frame_count = 1,
+      --line_length = 1,
+      --shift = util.by_pixel(0.5, -9),
+      scale = 0.5,
     },
   },
-  energy_source =
+
   {
-    type = "electric",
-    usage_priority = "secondary-input",
-    emissions_per_minute = { pollution = 4 }
-  },
-  energy_usage = "210kW",
-  crafting_categories = {"simulation_chamber"},
-  fluid_boxes =
-  {
-    --[[{
-      production_type = "input",
-      pipe_covers = pipecoverspictures(),
-      volume = 1000,
-      pipe_connections =
+    type = "furnace",
+    name = "simulation_chamber",
+    cant_insert_at_source_message_key = "inventory-restriction.cant-be-simulated",
+    icon = icons .. "simulation_chamber.png",
+    flags = {"placeable-neutral", "placeable-player", "player-creation", "not-rotatable"},
+    minable = {mining_time = 0.2, result = "simulation_chamber"},
+    fast_replaceable_group = "simulation_chamber",
+    max_health = 300,
+    corpse = "simulation_chamber-remnants",
+    dying_explosion = "chemical-plant-explosion",
+    icon_draw_specification = {shift = {0, -0.3}},
+    circuit_wire_max_distance = assembling_machine_circuit_wire_max_distance,
+    circuit_connector = circuit_connector_definitions.create_vector
+    (
+      universal_connector_template,
       {
-        {
-          flow_direction="input",
-          direction = defines.direction.north,
-          position = {-1, -1}
-        }
+        { variation = 27, main_offset = util.by_pixel(10.5, 61), shadow_offset = util.by_pixel(15.5, 66), show_shadow = false },
+        { variation = 27, main_offset = util.by_pixel(10.5, 61), shadow_offset = util.by_pixel(15.5, 66), show_shadow = false },
+        { variation = 27, main_offset = util.by_pixel(10.5, 61), shadow_offset = util.by_pixel(15.5, 66), show_shadow = false },
+        { variation = 27, main_offset = util.by_pixel(10.5, 61), shadow_offset = util.by_pixel(15.5, 66), show_shadow = false }
       }
-    },]]
-    --[[{
-      production_type = "input",
-      pipe_covers = pipecoverspictures(),
-      volume = 1000,
-      pipe_connections =
-      {
-        {
-          flow_direction="input",
-          direction = defines.direction.north,
-          position = {1, -1}
-        }
-      }
-    },]]
-    --[[
+    ),
+    collision_box = {{-(ent_size/2 -0.3), -(ent_size/2 -0.3)}, {(ent_size/2 -0.3), (ent_size/2 -0.3)}},
+    selection_box = {{-ent_size/2, -ent_size/2}, {ent_size/2, ent_size/2}},
+    damaged_trigger_effect = hit_effects.entity(),
+    drawing_box_vertical_extension = 0.4,
+    --module_slots = 3,
+    allowed_effects = {"speed", "pollution", "quality"},
+    source_inventory_size = 1,
+    result_inventory_size = 6,
+    crafting_speed = 0.1,
+
+    graphics_set =
     {
-      production_type = "output",
-      pipe_covers = pipecoverspictures(),
-      volume = 100,
-      pipe_connections =
+      animation = 
+      {
+        layers =
+        {
+          {
+            filename = entity .. "simulation_chamber/simulation_chamber_base.png",
+            width = 512,
+            height = 512,
+            --frame_count = 1,
+            --line_length = 1,
+            --shift = util.by_pixel(0.5, -9),
+            scale = 0.5,
+          },
+          {
+            filename = entity .. "simulation_chamber/simulation_chamber_shadow.png",
+            width = 512,
+            height = 512,
+            --frame_count = 1,
+            --line_length = 1,
+            --shift = util.by_pixel(0.5, -9),
+            scale = 0.5,
+            draw_as_shadow = true,
+          },
+        }
+      },
+      working_visualisations =
       {
         {
-          flow_direction = "output",
-          direction = defines.direction.south,
-          position = {-1, 1}
-        }
-      }
-    },]]
-    --[[
-    {
-      production_type = "output",
-      pipe_covers = pipecoverspictures(),
-      volume = 100,
-      pipe_connections =
-      {
+          animation =
+          {
+            filename = entity .. "simulation_chamber/simulation_chamber_screen_anim.png",
+            width = 64,
+            height = 64,
+            frame_count = 255, -- max is 255
+            line_length = 32,
+            animation_speed = 1,
+            shift = util.by_pixel_hr(-128, -112),
+            scale = 0.5,
+            draw_as_glow = true,
+          },
+        },
         {
-          flow_direction = "output",
-          direction = defines.direction.south,
-          position = {1, 1}
-        }
+          animation =
+          {
+            filename = entity .. "simulation_chamber/simulation_chamber_glow.png",
+            width = 512,
+            height = 512,
+            --frame_count = 255, -- max is 255
+            --line_length = 32,
+            --animation_speed = 0.04,
+            --shift = util.by_pixel_hr(-128, -112),
+            scale = 0.5,
+            draw_as_glow = true,
+            blend_mode = "additive",
+          },
+        },
+        {
+          apply_recipe_tint = "primary",
+          animation =
+          {
+            filename = entity .. "simulation_chamber/layer1.png",
+            width = 512,
+            height = 512,
+            scale = 0.5,
+          },
+        },
+        {
+          apply_recipe_tint = "secondary",
+          animation =
+          {
+            filename = entity .. "simulation_chamber/layer2.png",
+            width = 512,
+            height = 512,
+            scale = 0.5,
+          },
+        },
+        {
+          apply_recipe_tint = "tertiary",
+          animation =
+          {
+            filename = entity .. "simulation_chamber/layer3.png",
+            width = 512,
+            height = 512,
+            scale = 0.5,
+          },
+        },
+        {
+          apply_recipe_tint = "quaternary",
+          animation =
+          {
+            filename = entity .. "simulation_chamber/layer4.png",
+            width = 512,
+            height = 512,
+            scale = 0.5,
+          },
+        },
       }
-    }]]
-  },
-  --[[water_reflection =
-  {
-    pictures =
-    {
-      filename = "__base__/graphics/entity/chemical-plant/chemical-plant-reflection.png",
-      priority = "extra-high",
-      width = 28,
-      height = 36,
-      shift = util.by_pixel(5, 60),
-      variation_count = 4,
-      scale = 5
     },
-    rotate = false,
-    orientation_to_variation = true
-  }]]
-},
+    impact_category = "metal-large",
+    open_sound = {filename = "__base__/sound/open-close/fluid-open.ogg", volume = 0.55},
+    close_sound = {filename = "__base__/sound/open-close/fluid-close.ogg", volume = 0.54},
+    working_sound =
+    {
+      sound = {
+        filename = tssounds .. "simulation_chamber_working_sound.ogg",
+        volume = 0.7,
+      },
+      fade_in_ticks = 10,
+      fade_out_ticks = 40,
+      max_sounds_per_prototype = 4,
+      idle_sound = {
+        filename = tssounds .. "simulation_chamber_working_sound_idle.ogg",
+        volume = 0.7,
+      },
+    },
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions_per_minute = { pollution = 4 }
+    },
+    energy_usage = "210kW",
+    crafting_categories = {"simulation_chamber"},
+    fluid_boxes = {},
+  },
 })
